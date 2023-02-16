3b190b399d62de81f9e002fab90681d2e905d697
ahmadaaas <125567207+ahmadaaas@users.noreply.github.com>
2023-02-16 15:33:38 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 150)
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 70)
+basic.pause(2000)
+maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 150)
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 70)
+basic.pause(2000)
+basic.forever(function () {
+	
+})
